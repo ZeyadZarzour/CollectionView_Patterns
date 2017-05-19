54932e5fc9451109d6ec054621ec70111c22078d
Schnorts <Radbonus@apples-MacBook-Pro.local>
2017-05-19 16:47:34 +0200

    Initial Commit

---
+++ CollectionViewPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewPatterns
+//
+//  Created by Radbonus on 19.05.17.
+//  Copyright © 2017 Radbonus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
